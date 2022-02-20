63b090ca58978b861415ce6eab4d151c91ace4d1
martin-favre <martin.favre22@gmail.com>
2022-02-20 18:04:05 +0100

    Add vertical offset to camera

--- Assets/MetroidvaniaController/Scripts/Player/CameraFollow.cs
+++ Assets/MetroidvaniaController/Scripts/Player/CameraFollow.cs
@@ -4,53 +4,55 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-	public float FollowSpeed = 2f;
-	public Transform Target;
+    public float FollowSpeed = 2f;
+    public Transform Target;
 
-	// Transform of the camera to shake. Grabs the gameObject's transform
-	// if null.
-	private Transform camTransform;
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    private Transform camTransform;
 
-	// How long the object should shake for.
-	public float shakeDuration = 0f;
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
 
-	// Amplitude of the shake. A larger value shakes the camera harder.
-	public float shakeAmount = 0.1f;
-	public float decreaseFactor = 1.0f;
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.1f;
+    public float decreaseFactor = 1.0f;
+    public float verticalOffset = 3;
 
-	Vector3 originalPos;
+    Vector3 originalPos;
 
-	void Awake()
-	{
-		Cursor.visible = false;
-		if (camTransform == null)
-		{
-			camTransform = GetComponent(typeof(Transform)) as Transform;
-		}
-	}
+    void Awake()
+    {
+        Cursor.visible = false;
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
 
-	void OnEnable()
-	{
-		originalPos = camTransform.localPosition;
-	}
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
 
-	private void Update()
-	{
-		Vector3 newPosition = Target.position;
-		newPosition.z = -10;
-		transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    private void Update()
+    {
+        Vector3 newPosition = Target.position;
+        newPosition.z = -10;
+        newPosition.y += verticalOffset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
 
-		if (shakeDuration > 0)
-		{
-			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
 
-			shakeDuration -= Time.deltaTime * decreaseFactor;
-		}
-	}
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+    }
 
-	public void ShakeCamera()
-	{
-		originalPos = camTransform.localPosition;
-		shakeDuration = 0.2f;
-	}
+    public void ShakeCamera()
+    {
+        originalPos = camTransform.localPosition;
+        shakeDuration = 0.2f;
+    }
 }
